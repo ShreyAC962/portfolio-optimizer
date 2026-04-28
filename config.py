35662604615399e6fd50e393916f25b26264f664
Shreya C <97959780+ShreyAC962@users.noreply.github.com>
2026-04-27 21:19:40 -0700

    Added configuration logic

---
+++ config.py
@@ -0,0 +1,7 @@
+DATA_PATH = "data/sample_data.csv"
+
+# All assets in dataset (you actually have 5 assets)
+ASSETS = ["AAPL", "GOOG", "MSFT", "AMZN", "TSLA"]
+
+# Training iterations for RL / ML models
+EPOCHS = 10
